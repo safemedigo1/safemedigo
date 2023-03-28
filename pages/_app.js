825dddf74010f32d47f35347714ddf27177be8ed
safemedigo1 <emarketing.ser@gmail.com>
2023-03-28 03:22:05 +0200

    add GTM code

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,36 @@ import { ContextProvider } from "@/context/store";
 import "@/styles/globals.scss";
 import NoSsr from "@mui/base/NoSsr";
 import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
+import Script from "next/script";
 import { Layout } from "../components";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ContextProvider>
-      <NoSsr>
-        <CssBaseline />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </NoSsr>
-    </ContextProvider>
+    <>
+      <ContextProvider>
+        <NoSsr>
+          <CssBaseline />
+          <Layout>
+            <Head>
+              <title>Your title</title>
+              <meta name="description" content="Your description" />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K26NKMK');
+      `}
+            </Script>
+
+            <Component {...pageProps} />
+          </Layout>
+        </NoSsr>
+      </ContextProvider>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html lang="en">
-      {/* <Head /> */}
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -11,8 +11,32 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         ></link>
+
+        {/* GTM container code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K26NKMK');
+          <!-- End Google Tag Manager -->`,
+          }}
+        />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K26NKMK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
