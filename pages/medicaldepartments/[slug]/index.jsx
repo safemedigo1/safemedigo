f88b498827b027d7f2f3215b2a198db3fa1fb6ee
safemedigo1 <emarketing.ser@gmail.com>
2023-05-30 23:59:56 +0300

    ..

--- pages/medicaldepartments/[slug]/index.jsx
+++ pages/medicaldepartments/[slug]/index.jsx
@@ -529,7 +529,7 @@ export async function getStaticProps({ params, locale }) {
       ...(await serverSideTranslations(locale, ['navbar', 'sec_navbar', "contact_details", 'blogs_page', 'page_header_comp', "most_popular", "proceduresSymptoms"])),
 
     },
-    revalidate: 10,
+    revalidate: 0,
   }
 }
 
