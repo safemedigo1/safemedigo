ab524f70821b0d33942bf051f5e1fb77d756ef5e
safemedigo1 <emarketing.ser@gmail.com>
2023-10-16 00:25:20 +0300

    ..

--- components/Slick/index.jsx
+++ components/Slick/index.jsx
@@ -109,11 +109,8 @@ const Slick = () => {
             {!isDeleteActive &&
               <motion.div
                 animate={{ opacity: 1 }}
-
                 initial={{ opacity: 0 }}
                 transition={{ duration: 1, }}
-
-
                 className={styles.img_container}>
                 <Image width={72} height={106} src={post3.src} alt={"doc.name"} />
 
@@ -150,6 +147,91 @@ const Slick = () => {
             }
 
           </div>
+          <div className={styles.box} onClick={() => setIsDeleteActive((prev) => !prev)}>
+            {!isDeleteActive &&
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1, }}
+                className={styles.img_container}>
+                <Image width={72} height={106} src={post3.src} alt={"doc.name"} />
+
+              </motion.div>
+            }
+            <motion.div
+              animate={{ x: isDeleteActive ? 0 : 20 }}
+              transition={{ type: 'spring', stiffness: 200, damping: 20, duration: 0.5 }}
+
+              className={styles.doc_info}>
+
+              <div className={styles.doc_name}><Typography>Dr Mehmet Nu.</Typography></div>
+              <div className={styles.rating}>
+                <Rating name="read-only" defaultValue={4} size="small" readOnly />
+                <span className={styles.reviews_num}>{4} Reviews</span>
+              </div>
+
+            </motion.div>
+            <motion.div
+              animate={{ x: isDeleteActive ? 10 : 20 }}
+              // transition={{ type: 'spring', stiffness: 200, damping: 20, duration: 0.5 }}
+              transition={{ type: 'spring', stiffness: 200, damping: 20, duration: 0.5 }}
+
+
+              className={`${styles.filterby}`}>
+              <Typography>5 Years</Typography>
+            </motion.div>
+
+            {isDeleteActive &&
+              <motion.div
+                className={styles.delete_btn}>
+                <button>Delete</button>
+              </motion.div>
+            }
+
+          </div>
+          <div className={styles.box} onClick={() => setIsDeleteActive((prev) => !prev)}>
+            {!isDeleteActive &&
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1, }}
+                className={styles.img_container}>
+                <Image width={72} height={106} src={post3.src} alt={"doc.name"} />
+
+              </motion.div>
+            }
+            <motion.div
+              animate={{ x: isDeleteActive ? 0 : 20 }}
+              transition={{ type: 'spring', stiffness: 200, damping: 20, duration: 0.5 }}
+
+              className={styles.doc_info}>
+
+              <div className={styles.doc_name}><Typography>Dr Mehmet Nu.</Typography></div>
+              <div className={styles.rating}>
+                <Rating name="read-only" defaultValue={4} size="small" readOnly />
+                <span className={styles.reviews_num}>{4} Reviews</span>
+              </div>
+
+            </motion.div>
+            <motion.div
+              animate={{ x: isDeleteActive ? 10 : 20 }}
+              // transition={{ type: 'spring', stiffness: 200, damping: 20, duration: 0.5 }}
+              transition={{ type: 'spring', stiffness: 200, damping: 20, duration: 0.5 }}
+
+
+              className={`${styles.filterby}`}>
+              <Typography>5 Years</Typography>
+            </motion.div>
+
+            {isDeleteActive &&
+              <motion.div
+                className={styles.delete_btn}>
+                <button>Delete</button>
+              </motion.div>
+            }
+
+          </div>
+
         </div>
       </div>
 
